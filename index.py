56f5d1b5e635a0fdcfe214799abd9433ba193174
cyan-wings <matthewyeow.yeow@gmail.com>
2023-04-21 16:48:04 +0800

    Changes for deployment

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import dill as pickle
 import os
 import shutil
 import pandas as pd
+import shutil
 
 def remove_columns(df):
    #Uses for loop to skip any error in dropping columns that don't exist (Some files may not have columns with a subset of these columns)
@@ -17,23 +18,38 @@ def remove_columns(df):
     
    return df
 
-model = pickle.load(open('model.pkl','rb'))
-sample_table = [('apache', 1), ('testing', 2)]
 rankingData = pd.read_csv("forWebsite.csv", sep=",", encoding='utf-8')
+
+classifier_dict = {
+   "Default": "Default",
+   "RF": "Random Forest",
+   "MLP": "Multi-Layer Perceptron",
+   "SVM": "Support Vector Machine (RBF)",
+   "KNN": "K-Nearest Neighbors",
+   "SGD": "Logistic Regression",
+   "DT": "Decision Trees",
+   "R": "Ridge Regression",
+   "XGB": "Extreme Gradient Boosting",
+   "GPC": "Gaussian Process",
+   "ADA": "Adaptive Boosting",
+   "GB": "Gradient Boosting"
+}
 
 app = Flask(__name__)
 @app.route('/')
 def indexPage(): 
    return githubSearchPage()
 
-@app.route('/github_search')
+@app.route('/github_search', methods = ['GET'])
 def githubSearchPage():
-   return render_template('index.html')
+   return render_template('index.html', currentModel = "Default", modelData=classifier_dict)
 
 @app.route('/ranking', methods = ['GET', 'POST'])
 def rankingPage():
+   currentTag = 'All'
    select = request.form.get('tag')
-   #print(select)
+   if select != None:
+      currentTag = select
 
    if select != 'All' and select != None:
       sortedRanking = rankingData[rankingData[select] == 1].sort_values(by=['predicted'], ascending=False).reset_index()
@@ -47,7 +63,7 @@ def rankingPage():
    tagsForSelection = []
    for t in rankingData.iloc[:, 4:-2].columns:
       tagsForSelection.append(t)
-   return render_template('ranking.html', data = sorted(tagsForSelection), tag = select, rankingTable = sortedRanking.head(10))
+   return render_template('ranking.html', currentTag = currentTag, data = sorted(tagsForSelection), tag = select, rankingTable = sortedRanking.head(10))
 
 @app.route('/visualisations')
 def visualisationsPage():
@@ -59,16 +75,26 @@ def aboutPage():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-   result = ""
-   for key, value in request.form.items():
-      result = value
-      print((key, value))
+   currentModel = "Default"
 
-   token = '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'
-   flag = os.system("git clone https://{}:x-oauth-basic@github.com/{}".format(token, result))
-   #print(flag)
-   flag = os.system("SourceMeter-9.1.1/Java/SourceMeterJava " + "-projectName=test " + "-resultsDir=r -projectBaseDir={} ".format(result.split('/')[1]) + "-runAndroidHunter=false -runMetricHunter=false -runVulnerabilityHunter=false -runFaultHunter=false -runRTEHunter=false -runDCF=false -runMET=true -currentDate=none -runPMD=false -runFB=false")
-   error_array = []
+   gitLink = request.form["githublink"]
+   selectedModel = request.form["modelSelect"]
+   if selectedModel == "Default":
+      selectedModel = "R"
+   else:
+      currentModel = classifier_dict[selectedModel]
+   model = pickle.load(open('Saved_Models/{}.pkl'.format(selectedModel),'rb'))
+
+   token = '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'
+   flag = os.system("git clone https://{}:x-oauth-basic@github.com/{}".format(token, gitLink))
+
+   if flag==32768:
+      shutil.rmtree(gitLink.split('/')[1])
+      return render_template('index.html', modelData=classifier_dict, pre_text='Input a valid GitHub Link')
+
+   flag = os.system("SourceMeter-9.1.1/Java/SourceMeterJava " + "-projectName=test " + "-resultsDir=r -projectBaseDir={} ".format(gitLink.split('/')[1]) + "-runAndroidHunter=false -runMetricHunter=false -runVulnerabilityHunter=false -runFaultHunter=false -runRTEHunter=false -runDCF=false -runMET=true -currentDate=none -runPMD=false -runFB=false")
+   #except IndexError:
+      #return render_template('index.html', modelData=classifier_dict, pre_text='Input a valid GitHub Link')
         
    if flag == 0:
 
@@ -109,14 +135,18 @@ def predict():
    #print(sample_array['No_F'])
    sample_array = sample_array.drop(['Cmin_NOC', 'Cmed_NOC', 'Cmin_NOD', 'Cmed_NOD', 'Cmin_NLPA', 'Cmin_NLS', 'Cmin_NPA', 'Cmin_NS', 'Cmin_TNLPA', 'Cmin_TNLS', 'Cmin_TNPA', 'Cmin_TNS', 'Fmin_CLOC', 'Fmed_CLOC', 'Fmax_CLOC', 'Fsum_CLOC', 'Fstd_CLOC', 'Mmin_NL', 'Mmin_NLE', 'Mmin_NII', 'Mmin_NOI', 'Mmin_CD', 'Mmin_CLOC', 'Mmin_DLOC', 'Mmin_TCD', 'Mmin_TCLOC', 'Cmin_CLOC', 'Cmin_DLOC', 'Cmin_TCLOC', 'Cmed_NOP', 'Cstd_NOP', 'Cmed_NLG', 'Cmed_TNLG', 'Mmin_HPL', 'Mmin_HPV', 'Mmin_HTRP', 'Mmin_MI'])
    #print(model.clf.predict([sample_array]))
-   
+
+   if sample_array.No_C == 0:
+      shutil.rmtree(gitLink.split('/')[1])
+      return render_template('index.html', modelData=classifier_dict, pre_text='Input a valid Java Project')
+
    if model.clf.predict([sample_array])[0] == 1:
       prediction = Markup('<mark style="background-color:initial;color:#090" class="has-inline-color">HIGH</mark>')
    else:
       prediction = Markup('<mark style="background-color:initial;color:#FF0000" class="has-inline-color">LOW</mark>')
 
    status_array = []
-   importance_features = [('No_F', 250, 'L'), ('Cstd_CBO', 2, 7), ('Csum_NL', 450, 'L'), ('Fsum_PUA', 100, 'H'), ('Cstd_CBOI', 2, 11), ('Cmax_NII', 200, 'L'), ('Cmax_CBO', 20, 80)]
+   importance_features = [('Fsum_PUA', 40, 'H'), ('No_F', 250, 'L'), ('Cmax_NII', 80, 'L'), ('Csum_NL', 450, 'L'), ('Cstd_CBO', 2, 7),  ('Cmax_LCOM5', 10, 'H'), ('Cmax_CBO', 20, 80)]
    actual_values = []
    for f in importance_features:
       actual_values.append(sample_array[f[0]])
@@ -157,43 +187,43 @@ def predict():
                 <tbody>
                   <tr>
                     <th scope="row">1</th>
-                    <td>Number of files</td>
-                    <td>< 250</td>
+                    <td>PUA - File (sum)</td>
+                    <td>> 40</td>
                     <td>{actual[0]}</td>
                     <td><i class="{status[0]}"/></td>
                   </tr>
                   <tr>
                     <th scope="row">2</th>
-                    <td>Cstd_CBO</td>
-                    <td>2 - 7</td>
+                    <td>Number of files</td>
+                    <td>< 250</td>
                     <td>{actual[1]}</td>
                     <td><i class="{status[1]}"/></td>
                   </tr>
                   <tr>
                     <th scope="row">3</th>
-                    <td>Csum_NL</td>
-                    <td>< 450</td>
+                    <td>NII - Class (max)</td>
+                    <td>< 80</td>
                     <td>{actual[2]}</td>
                     <td><i class="{status[2]}"/></td>
                   </tr>
                   <tr>
                     <th scope="row">4</th>
-                    <td>Fsum_PUA</td>
-                    <td>> 100</td>
+                    <td>NL - Class (sum)</td>
+                    <td>< 450</td>
                     <td>{actual[3]}</td>
                     <td><i class="{status[3]}"/></td>
                   </tr>
                   <tr>
                     <th scope="row">5</th>
-                    <td>Cstd_CBOI</td>
-                    <td>2 - 11</td>
+                    <td>CBO - Class (std)</td>
+                    <td>2 - 7</td>
                     <td>{actual[4]}</td>
                     <td><i class="{status[4]}"/></td>
                   </tr>
                   <tr>
                     <th scope="row">6</th>
-                    <td>Cmax_NII</td>
-                    <td>< 200</td>
+                    <td>LCOM5 - Class (max)</td>
+                    <td>> 10</td>
                     <td>{actual[5]}</td>
                     <td><i class="{status[5]}"/></td>
                   </tr>
@@ -210,7 +240,9 @@ def predict():
          </div>
    '''.format(actual=actual_values, status=status_array))
 
-   return render_template('index.html', pre_text='Reusability is ', prediction_text = prediction, table_display=table_features)
+   shutil.rmtree(gitLink.split('/')[1])
+
+   return render_template('index.html', currentModel = currentModel, modelData=classifier_dict, pre_text='Reusability is ', prediction_text = prediction, table_display=table_features)
 
 @app.route('/ranking_result', methods = ['POST'])
 
